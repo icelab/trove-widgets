b8d02ea63c9feb15b9b54912a1a6a1961c82f4d3
query-string <atimofeev@reactant.ru>
2014-06-19 08:31:39 +0400

    Specify Heroku related assets compiling options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  config.assets.initialize_on_precompile = false
+  config.assets.compress = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
