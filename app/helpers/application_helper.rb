a77607b453870d59c670d51788ae706118f63b59
query-string <atimofeev@reactant.ru>
2014-06-24 23:47:38 +0400

    Convert counters to human readable format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     params.except(:controller)
   end
 
+  def human_counts(word, count)
+    "#{number_to_human(count, format: '%n%u', units: {thousand: 'K'})} #{word.pluralize(count)}"
+  end
+
 end
