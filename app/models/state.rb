a8cb536a059540c0943b2f1c39874a251b1c84c8
query-string <atimofeev@reactant.ru>
2014-06-26 01:26:08 +0400

    Add reloations between States and Titles

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,5 +1,6 @@
 class State < ActiveRecord::Base
 
   self.primary_key = :abbrev
+  has_many :titles, foreign_key: :state_abbrev, class_name: Title
 
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,6 @@
 class Title < ActiveRecord::Base
+
+  self.primary_key = :trove_id
+  belongs_to :state, class_name: State
+
 end
