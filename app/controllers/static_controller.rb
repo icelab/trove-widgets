b3bf2ab9491f9475c81bd78140e43a2c332bd6ff
query-string <atimofeev@reactant.ru>
2014-06-30 14:16:57 +0400

    Widget builder wireframing

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,1 +1,8 @@
-class StaticController < ApplicationController; end
+class StaticController < ApplicationController
+
+  def index
+    @states = State.order('name ASC')
+    @titles = Title.order('name ASC')
+  end
+
+end
