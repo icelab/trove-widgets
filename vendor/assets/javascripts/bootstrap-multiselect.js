a828207e6aae51b80b5ce3eef6682aa548482cee
query-string <atimofeev@reactant.ru>
2014-07-01 09:11:07 +0400

    Truncate selected title string

--- vendor/assets/javascripts/bootstrap-multiselect.js
+++ vendor/assets/javascripts/bootstrap-multiselect.js
@@ -143,7 +143,8 @@
 
                             selected += label + ', ';
                         });
-                        return selected.substr(0, selected.length - 2) + ' <b class="caret"></b>';
+                        //return selected.substr(0, selected.length - 2) + ' <b class="caret"></b>';
+                        return selected.substr(0, 35) + ' <b class="caret"></b>';
                     }
                 }
             },
