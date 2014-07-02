ca2bba1563394a3925a4e9e026a3db66272da0d9
query-string <atimofeev@reactant.ru>
2014-07-02 12:37:32 +0400

    Filter empty arrtibutes

--- app/assets/javascripts/application/application.js
+++ app/assets/javascripts/application/application.js
@@ -1,10 +1,11 @@
 //= require jquery
 //= require jquery.role.min
 //= require bootstrap
-//= require bootstrap-multiselect
+//= require bootstrap.multiselect
 
 //= require underscore
 //= require backbone
+//= require backbone.syphon.min
 //= require_tree ../../templates
 
 //= require_tree .
