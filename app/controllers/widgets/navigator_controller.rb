896d39de19014826e58898e74247602a687b9b60
query-string <atimofeev@reactant.ru>
2014-07-10 04:52:55 +0400

    Filter empty start_date and end_date values

--- app/controllers/widgets/navigator_controller.rb
+++ app/controllers/widgets/navigator_controller.rb
@@ -7,8 +7,8 @@ class Widgets::NavigatorController < ApplicationController
     @state = State.new.find_by_abbrev(response[:state]).name
     @newspapers = response[:titles]
     @issuecount = response[:total]
-    @start_date = @newspapers.map{|item| item.start_date}.min
-    @end_date = @newspapers.map{|item| item.end_date}.max
+    @start_date = @newspapers.map(&:start_date).delete_if{|value| value == '0'}.min
+    @end_date = @newspapers.map(&:end_date).delete_if{|value| value == '0'}.max
   end
 
 end
