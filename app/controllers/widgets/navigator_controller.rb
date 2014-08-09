247e4bd1722f0b756049935877d0262e17887d7c
query-string <atimofeev@reactant.ru>
2014-08-09 10:45:40 +0700

    Use local titles store for navigator widget

--- app/controllers/widgets/navigator_controller.rb
+++ app/controllers/widgets/navigator_controller.rb
@@ -3,12 +3,12 @@ class Widgets::NavigatorController < ApplicationController
   layout false
 
   def title
-    response = set_cache(['navigator_title', caching_params(params)]) {TroveApi.new.state(params[:state])}
-    @state = State.new.find_by_abbrev(response[:state]).name
-    @newspapers = response[:titles]
-    @issuecount = response[:total]
-    @start_date = @newspapers.map(&:start_date).delete_if{|value| value == '0'}.min
-    @end_date = @newspapers.map(&:end_date).delete_if{|value| value == '0'}.max
+    state = State.new.find_by_abbrev(params[:state])
+    @state = state.name
+    @newspapers = state.titles
+    @issuecount = @newspapers.size
+    @start_date = @newspapers.map(&:start_year).delete_if{|value| value == '0'}.min
+    @end_date = @newspapers.map(&:end_year).delete_if{|value| value == '0'}.max
   end
 
 end
