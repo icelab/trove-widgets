c0526dd388fc7354d5830a8fc7b9e7fea626efc6
query-string <atimofeev@reactant.ru>
2014-08-11 16:36:30 +0700

    Fix dates output for navigator widget

--- app/controllers/widgets/navigator_controller.rb
+++ app/controllers/widgets/navigator_controller.rb
@@ -7,8 +7,8 @@ class Widgets::NavigatorController < ApplicationController
     @state = state.name
     @newspapers = state.titles
     @issuecount = @newspapers.size
-    @start_date = @newspapers.map(&:start_year).delete_if{|value| value == '0'}.min
-    @end_date = @newspapers.map(&:end_year).delete_if{|value| value == '0'}.max
+    @start_year = @newspapers.map(&:start_year).delete_if{|value| value == '0'}.min
+    @end_year = @newspapers.map(&:end_year).delete_if{|value| value == '0'}.max
   end
 
 end
