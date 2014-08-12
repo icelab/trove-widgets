a3d074a1f25ba6e4739399cfe36369b349b61e2f
query-string <atimofeev@reactant.ru>
2014-08-12 16:26:22 +0700

    Use Unicorn as application server

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,15 +7,14 @@ role :web, 'trovespace.webfactional.com'
 role :app, 'trovespace.webfactional.com'
 
 set :deploy_to, "/home/trovespace/webapps/widgets"
-set :default_stage, "production"
+set :rails_env, "production"
 set :default_environment, {
   'PATH' => "#{deploy_to}/bin:$PATH",
   'GEM_HOME' => "#{deploy_to}/gems",
-  "RAILS_ENV" =>  "#{default_stage}"
+  "RAILS_ENV" =>  "#{rails_env}"
 }
 
 set :user, 'trovespace'
-set :rails_env, "#{default_stage}"
 set :use_sudo, false
 default_run_options[:pty] = true
 
@@ -34,9 +33,20 @@ namespace :deploy do
   puts " \\_____________________/ \n"
   puts "==================================================="
 
-  desc "Restart nginx"
+  #desc "Restart nginx"
+  #task :restart do
+  #  run "#{deploy_to}/bin/restart"
+  #end
+
+  desc "Server restart."
   task :restart do
-    run "#{deploy_to}/bin/restart"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
 
   desc "Bundle install gems"
@@ -47,7 +57,7 @@ namespace :deploy do
   namespace :assets do
     desc "Run the precompile task remotely"
     task :precompile, roles: :web, except: {no_release: true} do
-      run "cd #{deploy_to}/current; bundle exec rake assets:precompile RAILS_ENV=#{default_stage}"
+      run "cd #{deploy_to}/current; bundle exec rake assets:precompile RAILS_ENV=#{rails_env}"
     end
   end
 
